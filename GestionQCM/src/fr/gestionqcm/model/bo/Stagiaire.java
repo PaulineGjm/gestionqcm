95866df7bb90d34b1309cd88a0600a60d2a60972
d1410beauvillainc <d1410beauvillainc@21-UC36-12.ad.campus-eni.fr>
2015-10-20 11:35:43 +0200

    Ajout utilisateur + stagiaire

---
+++ GestionQCM/src/fr/gestionqcm/model/bo/Stagiaire.java
@@ -0,0 +1,15 @@
+package fr.gestionqcm.model.bo;
+
+public class Stagiaire extends Utilisateur{
+	
+	private int idPromotion;
+
+	public int getIdPromotion() {
+		return idPromotion;
+	}
+
+	public void setIdPromotion(int idPromotion) {
+		this.idPromotion = idPromotion;
+	}
+	
+}
