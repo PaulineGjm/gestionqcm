88ebdaf0a621af9486f5615618a7668e224f6b2f
d1311martelp <d1311martelp@21-UC36-03.ad.campus-eni.fr>
2015-10-27 09:31:00 +0100

    Mise en forme menu

--- GestionQCM/src/fr/gestionqcm/controler/teacher/themes/NewThemeServlet.java
+++ GestionQCM/src/fr/gestionqcm/controler/teacher/themes/NewThemeServlet.java
@@ -1,25 +1,25 @@
-package fr.gestionqcm.controler;
+package fr.gestionqcm.controler.teacher.themes;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import fr.gestionqcm.model.bo.Utilisateur;
+import fr.gestionqcm.model.bo.Theme;
+import fr.gestionqcm.model.handler.ThemeHandler;
 
 /**
- * Servlet implementation class RootServlet
+ * Servlet implementation class NewThemeServlet
  */
-public class RootServlet extends HttpServlet {
+public class NewThemeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public RootServlet() {
+	public NewThemeServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -44,22 +44,20 @@ public class RootServlet extends HttpServlet {
 
 	private void processRequest(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		Utilisateur user = (Utilisateur) request.getSession().getAttribute(
-				"user");
-		RequestDispatcher dispatcher = null;
-		String url = "/login";
 
-		if (user != null) {
-			if (user.isAnimateur())
-				url = "/teacher";
-			else if (user.isStagiaire())
-				url = "/trainee";
+		String label = request.getParameter("theme_label");
+		Theme theme = new Theme();
+		theme.setLabel(label);
+
+		try {
+			ThemeHandler.add(theme);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		// response.sendRedirect(request.getContextPath() + url);
-		request.setAttribute("homeactive", "active");
-		dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
+		response.sendRedirect(request.getContextPath() + "/ListThemes");
+
 	}
 
 }
